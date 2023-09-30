af9f0f0d442495c3e5d372d7e19e11435002dd0b
Sandip Bhatt <sandip0510@gmail.com>
2023-09-30 23:27:51 +0530

    Install the Custom Gutenberge Block

---
+++ wp-content/plugins/gutengerge-custom-block/gutenberge-custom-block.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+* Plugin Name: Custom Block
+* Author: Sandip Bhatt
+* Description: Create a custom Gutenberge Block
+*/
+
+function sb_gutenberge_block(){
+
+
+}
+add_action('enqueue_block_editor_assets','sb_gutenberge_block');
+?>
